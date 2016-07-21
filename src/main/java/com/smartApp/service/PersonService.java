01d39e0ef875735f17a8c26b04e8fae2a541aeee
Dilan Gamachchige <dgamachchige@isa.ae>
2016-07-21 18:43:24 +0530

    initial commit

---
+++ src/main/java/com/smartApp/service/PersonService.java
@@ -0,0 +1,15 @@
+package com.smartApp.service;
+
+import java.util.List;
+
+import com.smartApp.model.Person;
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
